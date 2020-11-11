64a379f0a4c94912bfff602ad84a94eb5e19529d
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-11-11 15:27:51 +0100

    Init post list

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ export default {
     ]
   },
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ["vue-essential-slices/src/styles/styles.scss"],
+  // css: ["vue-essential-slices/src/styles/styles.scss"],
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
   // Auto import components (https://go.nuxtjs.dev/config-components)
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,27 @@
 <template>
-  <h1>Nicole's site</h1>
+  <div>
+    <h1>Nicole's site</h1>
+    <ul>
+      <li v-for="article in document" :key="article.id">
+        <NuxtLink :to="'/' + article.uid">
+          <h2>{{ article.data.title }}</h2>
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  async asyncData({ $prismic, params, error }) {
+    const document = (await $prismic.api.query("")).results;
+    if (document) {
+      return { document };
+    } else {
+      error({ statusCode: 404, message: "Page not found" });
+    }
+  },
 };
 </script>
 
