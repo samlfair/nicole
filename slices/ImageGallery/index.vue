fc1e73a6a4f8ef4b294f0df5aa9c9199289d40a9
Sam Littlefair <sam.littlefair@prismic.io>
2020-12-18 18:41:36 +0100

    Add image gallery

---
+++ slices/ImageGallery/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <li v-for="item in slice.items" :key="item.image.url">
+      <img :src="item.image.square.url" :alt="item.image.alt" />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "ImageGrid",
+  props: {
+    slice: Object
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 100%;
+}
+
+ul {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 1rem;
+  padding: 2rem 0;
+}
+</style>
